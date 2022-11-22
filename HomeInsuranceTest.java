8418e3fd58ba8930eb1e2f0be189b2f42ecc4289
My-git-Mubeena <115867952+My-git-Mubeena@users.noreply.github.com>
2022-11-22 11:26:11 +0530

    Add files via upload
    This commit have all program files in TestNG package

---
+++ HomeInsuranceTest.java
@@ -0,0 +1,15 @@
+package Insurance;
+
+import org.testng.annotations.Test;
+
+public class HomeInsuranceTest {
+	@Test
+	public void print()
+	{
+		System.out.println("Hia");
+	}
+
+
+}
+
+ 
---
+++ LifeInsuranceTest.java
@@ -0,0 +1,12 @@
+package Insurance;
+
+import org.testng.annotations.Test;
+
+public class LifeInsuranceTest {
+	@Test
+	public void print()
+	{
+		System.out.println("HAI");
+	}
+
+}
